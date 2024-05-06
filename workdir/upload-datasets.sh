272972683d1a5f84ae2d97e2261c61a7d03ff9f3
jordi-h <hoorelbeke.jordi1704@gmail.com>
2024-05-06 20:53:55 +0200

    Output files added.

--- workdir/upload-datasets.sh
+++ workdir/upload-datasets.sh
@@ -6,6 +6,7 @@ CONTAINER_NAME="namenode"
 docker cp datasets/title.principals.tsv $CONTAINER_NAME:title.principals.tsv
 docker cp datasets/title.principals.millions.tsv $CONTAINER_NAME:title.principals.millions.tsv
 docker cp datasets/title.ratings.tsv $CONTAINER_NAME:title.ratings.tsv
+#docker cp datasets/smallest.tsv $CONTAINER_NAME:smallest.tsv
 
 # Create a directory in HDFS for storing the data
 docker exec namenode hdfs dfs -mkdir -p /data/imdb
@@ -14,3 +15,4 @@ docker exec namenode hdfs dfs -mkdir -p /data/imdb
 docker exec namenode hdfs dfs -put title.principals.tsv /data/imdb/title.principals.tsv
 docker exec namenode hdfs dfs -put title.principals.millions.tsv /data/imdb/title.principals.millions.tsv
 docker exec namenode hdfs dfs -put title.ratings.tsv /data/imdb/title.ratings.tsv
+#docker exec namenode hdfs dfs -put smallest.tsv /data/imdb/smallest.tsv
